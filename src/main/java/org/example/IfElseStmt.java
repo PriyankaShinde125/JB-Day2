9f9d5b68f62007b238b0740955ed530b6a5227f0
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-08 11:08:11 +0530

    ADDED: code for digit to word with if else

---
+++ src/main/java/org/example/IfElseStmt.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class IfElseStmt {
+    private int number;
+    Scanner sc;
+
+    public IfElseStmt() {
+        sc = new Scanner(System.in);
+    }
+
+    public void numberToWord() {
+        System.out.println("Enter single digit number");
+        number = sc.nextInt();
+        if (number == 0)
+            System.out.println("Zero");
+        else if (number == 1)
+            System.out.println("one");
+        else if (number == 2)
+            System.out.println("Two");
+        else if (number == 3)
+            System.out.println("Three");
+        else if (number == 4)
+            System.out.println("Four");
+        else if (number == 5)
+            System.out.println("Five");
+        else if (number == 6)
+            System.out.println("Six");
+        else if (number == 7)
+            System.out.println("Seven");
+        else if (number == 8)
+            System.out.println("Eight");
+        else if (number == 9)
+            System.out.println("Nine");
+        else
+            System.out.println("Invalid input");
+
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //1 If_Else statement
+        IfElseStmt ifElseStmtObj=new IfElseStmt();
+        //1.1 Number to word
+        ifElseStmtObj.numberToWord();
+    }
+
+}
